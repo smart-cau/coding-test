1bb80201841d617a5d390c6bdd19510ba5fe18d5
Jung Hyun Woo <1995hyunwoo@naver.com>
2023-11-26 07:44:48 +0000

    백준 18111번. 마인크래프트

--- Test/src/bruteForce/p1182/Main.java
+++ Test/src/bruteForce/p1182/Main.java
@@ -10,7 +10,10 @@ public class Main {
     static int N;
     static int M;
     static int B;
-    static int[][] heights;
+    static int[][] ground;
+    static int totalBlocks = 0;
+    static int minTime = Integer.MAX_VALUE;
+    static int highest = 0;
     static FastReader scan = new FastReader();
 
     public static void main(String[] args) {
@@ -22,15 +25,49 @@ public class Main {
         N = scan.nextInt();
         M = scan.nextInt();
         B = scan.nextInt();
-        heights = new int[N][M];
+        totalBlocks += B;
+        ground = new int[N][M];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                heights[N][M] = scan.nextInt();
+                int height = scan.nextInt();
+                ground[i][j] = height;
+                totalBlocks += height;
             }
         }
     }
 
     static void solution() {
+        int mean = totalBlocks / (N * M);
+        int sameTimeCount = 0;
+        while (true) {
+            int time = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    int blocks = ground[i][j];
+                    if (blocks > mean)
+                        time += (blocks - mean) * 2;
+                    if (blocks < mean)
+                        time += mean - blocks;
+                }
+            }
+            // 평균이 9일 때랑 10일 때랑 걸리는 시간이 같다면??
+            if (minTime == time) {
+                ++sameTimeCount;
+            }
+            if (minTime > time) {
+                minTime = time;
+                sameTimeCount = 0;
+            }
+            if (minTime < time) {
+                highest = mean + 1;
+                if (sameTimeCount != 0) {
+                    highest += sameTimeCount;
+                }
+                break;
+            }
+            --mean;
+        }
+        System.out.println(minTime + " " + highest);
     }
 
     static class FastReader {
