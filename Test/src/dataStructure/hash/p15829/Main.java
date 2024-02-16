26037457fb034e81dd17f2d2412042f900294f24
Jung Hyun Woo <1995hyunwoo@naver.com>
2024-02-16 13:20:37 +0000

    백준 15829번. Hashing

--- Test/src/dataStructure/hash/p15829/Main.java
+++ Test/src/dataStructure/hash/p15829/Main.java
@@ -3,16 +3,16 @@
 // bronze 2
 package dataStructure.hash.p15829;
 
-import java.util.HashMap;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 class Main {
 
     static Scanner sc = new Scanner(System.in);
     static int n;
+    static final int M = 1234567891;
     static String str;
-    static HashMap<Character, Integer> charToInt = new HashMap<>();
-    static long result = 0;
+    static BigInteger result = new BigInteger("0");
     static int MAGIC_NUMBER = 31;
 
     public static void main(String[] args) {
@@ -27,21 +27,13 @@ class Main {
     }
 
     static void solution() {
-        initCharToInt();
         for (int i = 0; i < n; i++) {
-            char character = str.charAt(i);
-            int mappingValue = charToInt.get(character);
-            result += mappingValue * powerMagicNumber(i);
+            int charValue = str.charAt(i) - 'a' + 1;
+
+            result = result.add(BigInteger.valueOf(charValue).multiply(BigInteger.valueOf(31).pow(i)));
         }
 
-        System.out.println(result);
-    }
-
-    static void initCharToInt() {
-        int aInASCII = 97;
-        for (int mappingValue = 1; mappingValue <= 26; mappingValue++) {
-            charToInt.put((char) (aInASCII + mappingValue - 1), mappingValue);
-        }
+        System.out.println(result.remainder(BigInteger.valueOf(M)));
     }
 
     static long powerMagicNumber(long times) {
